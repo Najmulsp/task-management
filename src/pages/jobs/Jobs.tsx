5060d63979e301ec47af8e695237e3972dec33cb
Najmul <mdnajmulsarker1595@gmail.com>
2025-01-12 23:38:58 +0600

    added accordian

--- src/pages/jobs/Jobs.tsx
+++ src/pages/jobs/Jobs.tsx
@@ -56,15 +56,29 @@ const Jobs = () => {
         </section>
 
         {/* Accordion */}
-        <section>
-          <Accordion type="single" collapsible>
-            <AccordionItem value="item-1">
-              <AccordionTrigger>Is it accessible?</AccordionTrigger>
-              <AccordionContent>
-                Yes. It adheres to the WAI-ARIA design pattern.
-              </AccordionContent>
-            </AccordionItem>
-          </Accordion>
+        <section className="w-[840px] mx-auto">
+          <h1 className="text-4xl font bold text-center py-4">Your question and our answer</h1>
+        <Accordion type="single" collapsible className="w-full">
+      <AccordionItem value="item-1">
+        <AccordionTrigger>Is it accessible?</AccordionTrigger>
+        <AccordionContent>
+          Yes. It adheres to the WAI-ARIA design pattern.
+        </AccordionContent>
+      </AccordionItem>
+      <AccordionItem value="item-2">
+        <AccordionTrigger>Is it styled?</AccordionTrigger>
+        <AccordionContent>
+          Yes. It comes with default styles that matches the other
+          components&apos; aesthetic.
+        </AccordionContent>
+      </AccordionItem>
+      <AccordionItem value="item-3">
+        <AccordionTrigger>Is it animated?</AccordionTrigger>
+        <AccordionContent>
+          Yes. It's animated by default, but you can disable it if you prefer.
+        </AccordionContent>
+      </AccordionItem>
+    </Accordion>
         </section>
 
         <OurClients />
