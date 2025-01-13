2cf3cb47c6957dd35ac6d62d2eec81330109aa56
Najmul <mdnajmulsarker1595@gmail.com>
2025-01-13 23:22:45 +0600

    added rechart

--- src/pages/jobs/Jobs.tsx
+++ src/pages/jobs/Jobs.tsx
@@ -16,19 +16,45 @@ import {
   AccordionItem,
   AccordionTrigger,
 } from "@/components/ui/accordion";
+import { Bar, BarChart } from "recharts";
+
+import { ChartConfig, ChartContainer } from "@/components/ui/chart";
 
 const Jobs = () => {
+  const chartData = [
+    { month: "January", desktop: 186, mobile: 80 },
+    { month: "February", desktop: 305, mobile: 200 },
+    { month: "March", desktop: 237, mobile: 120 },
+    { month: "April", desktop: 73, mobile: 190 },
+    { month: "May", desktop: 209, mobile: 130 },
+    { month: "June", desktop: 214, mobile: 140 },
+  ];
+
+  const chartConfig = {
+    desktop: {
+      label: "Desktop",
+      color: "#2563eb",
+    },
+    mobile: {
+      label: "Mobile",
+      color: "#60a5fa",
+    },
+  } satisfies ChartConfig;
   return (
     <div className="grid-background w-full max-w-[1440px] mx-auto">
       <JobHeader />
       <div>
-        <img src={banner} className="w-full mx-auto border mb-10" alt="Banner" />
+        <img
+          src={banner}
+          className="w-full mx-auto border mb-10"
+          alt="Banner"
+        />
       </div>
       <main>
         {/* card section */}
         <section className="grid grid-cols-1 md:grid-cols-2 gap-4">
           <div className="mx-auto">
-            <Card>
+            <Card className="col-span-1">
               <CardHeader>
                 <CardTitle>Card Title</CardTitle>
                 <CardDescription>Card Description</CardDescription>
@@ -57,28 +83,40 @@ const Jobs = () => {
 
         {/* Accordion */}
         <section className="w-[840px] mx-auto">
-          <h1 className="text-4xl font bold text-center py-4">Your question and our answer</h1>
-        <Accordion type="single" collapsible className="w-full">
-      <AccordionItem value="item-1">
-        <AccordionTrigger>Is it accessible?</AccordionTrigger>
-        <AccordionContent>
-          Yes. It adheres to the WAI-ARIA design pattern.
-        </AccordionContent>
-      </AccordionItem>
-      <AccordionItem value="item-2">
-        <AccordionTrigger>Is it styled?</AccordionTrigger>
-        <AccordionContent>
-          Yes. It comes with default styles that matches the other
-          components&apos; aesthetic.
-        </AccordionContent>
-      </AccordionItem>
-      <AccordionItem value="item-3">
-        <AccordionTrigger>Is it animated?</AccordionTrigger>
-        <AccordionContent>
-          Yes. It's animated by default, but you can disable it if you prefer.
-        </AccordionContent>
-      </AccordionItem>
-    </Accordion>
+          <h1 className="text-4xl font bold text-center py-4">
+            Your question and our answer
+          </h1>
+          <Accordion type="single" collapsible className="w-full">
+            <AccordionItem value="item-1">
+              <AccordionTrigger>Is it accessible?</AccordionTrigger>
+              <AccordionContent>
+                Yes. It adheres to the WAI-ARIA design pattern.
+              </AccordionContent>
+            </AccordionItem>
+            <AccordionItem value="item-2">
+              <AccordionTrigger>Is it styled?</AccordionTrigger>
+              <AccordionContent>
+                Yes. It comes with default styles that matches the other
+                components&apos; aesthetic.
+              </AccordionContent>
+            </AccordionItem>
+            <AccordionItem value="item-3">
+              <AccordionTrigger>Is it animated?</AccordionTrigger>
+              <AccordionContent>
+                Yes. It's animated by default, but you can disable it if you
+                prefer.
+              </AccordionContent>
+            </AccordionItem>
+          </Accordion>
+        </section>
+        {/* chart */}
+        <section>
+          <ChartContainer config={chartConfig} className="min-h-[200px] w-full">
+            <BarChart accessibilityLayer data={chartData}>
+              <Bar dataKey="desktop" fill="var(--color-desktop)" radius={4} />
+              <Bar dataKey="mobile" fill="var(--color-mobile)" radius={4} />
+            </BarChart>
+          </ChartContainer>
         </section>
 
         <OurClients />
